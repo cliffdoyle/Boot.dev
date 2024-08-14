b92e2d99f63df9ea46ff1d95fcc74f8801bde7d5
Cliffdoyle <oyoocliff471@gmail.com>
2024-08-14 10:32:26 +0300

    Added middleware

--- main.go
+++ main.go
@@ -1,23 +1,61 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	// "net/url"
+)
 
-func Check(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type","text/plain;charset=utf-8")
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		fmt.Println(cfg.fileserverHits)
+		next.ServeHTTP(w, r)
+	})
+
+}
+
+func Check(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain;charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+
+func (cfg *apiConfig) Metrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain;charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits)
+}
+
+
+func (cfg *apiConfig) Reset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.Header().Set("Content-Type", "text/plain;charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
 
 func main() {
+	// newApp()
 	mux := http.NewServeMux()
+	apiCfg := apiConfig{}
+
+	// wrapMux := apiConfig.middlewareMetricsInc(mux)
 
 	serv := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	mux.HandleFunc("/healthz",Check)
-	fs:=http.FileServer(http.Dir("./assets"))
-	mux.Handle("/app/",http.StripPrefix("/app",fs))
+	mux.HandleFunc("/healthz", Check)
+	mux.HandleFunc("/metrics", apiCfg.Metrics)
+	mux.HandleFunc("/reset", apiCfg.Reset)
+	fs := http.FileServer(http.Dir("./assets"))
 
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fs)))
 	serv.ListenAndServe()
 }
