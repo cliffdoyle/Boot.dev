e74c75f8ad4a957ff9d547f191dafd16a3b06c25
Cliffdoyle <oyoocliff471@gmail.com>
2024-08-14 08:25:47 +0300

    added a healthz route to begin traffic to the server

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "net/http"
 
+func Check(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","text/plain;charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 
@@ -9,6 +15,9 @@ func main() {
 		Addr:    ":8080",
 		Handler: mux,
 	}
+	mux.HandleFunc("/healthz",Check)
+	fs:=http.FileServer(http.Dir("./assets"))
+	mux.Handle("/app/",http.StripPrefix("/app",fs))
 
 	serv.ListenAndServe()
 }
