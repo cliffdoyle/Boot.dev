3f8deb8620a8e42686ac22beb28e8af30d00649d
Cliffdoyle <oyoocliff471@gmail.com>
2024-08-14 12:10:40 +0300

    added html for metrics page

--- main.go
+++ main.go
@@ -20,18 +20,43 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func Check(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain;charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	if r.Method == http.MethodGet{
+
+		w.Header().Set("Content-Type", "text/plain;charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 }
 
-
 func (cfg *apiConfig) Metrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain;charset=utf-8")
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html;charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits)
 }
 
+func (cfg *apiConfig) Metrics2(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html;charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	htmlTemplate :=`<html>
+
+	<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+	</body>
+	
+	</html>`
+	fmt.Fprintf(w, htmlTemplate, cfg.fileserverHits)
+}
 
 func (cfg *apiConfig) Reset(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits = 0
@@ -43,7 +68,7 @@ func (cfg *apiConfig) Reset(w http.ResponseWriter, r *http.Request) {
 func main() {
 	// newApp()
 	mux := http.NewServeMux()
-	apiCfg := apiConfig{}
+	apiCfg := &apiConfig{}
 
 	// wrapMux := apiConfig.middlewareMetricsInc(mux)
 
@@ -51,9 +76,10 @@ func main() {
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	mux.HandleFunc("/healthz", Check)
-	mux.HandleFunc("/metrics", apiCfg.Metrics)
-	mux.HandleFunc("/reset", apiCfg.Reset)
+	mux.HandleFunc("/api/healthz", Check)
+	//mux.HandleFunc("/api/metrics", apiCfg.Metrics)
+	mux.HandleFunc("/admin/metrics", apiCfg.Metrics2)
+	mux.HandleFunc("/api/reset", apiCfg.Reset)
 	fs := http.FileServer(http.Dir("./assets"))
 
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fs)))
